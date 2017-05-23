f01d07df3b5edf0ee68b75d50e6e7b8dd4684af9
microsoftv3 <stronghuman0207@gmail.com>
2017-05-23 13:13:07 +0300

    Media changed

--- CmsCoreV2/Areas/CmsCore/Controllers/ControllerBase.cs
+++ CmsCoreV2/Areas/CmsCore/Controllers/ControllerBase.cs
@@ -5,11 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Options;
 
 namespace CmsCoreV2.Areas.CmsCore.Controllers
 {
     public class ControllerBase : Controller
     {
+        protected string AssetsUrl;
+        protected string UploadPath;
+
+
+        public ControllerBase()
+        {
+
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {             
+            ViewBag.UploadPath = "C:\\Users\\Admin\\Source\\Repos\\CmsCoreV2\\CmsCoreV2\\-2017\\";
+            base.OnActionExecuting(filterContext);
+
+        }
+
+
+
         [HttpPost]
         public IActionResult SetLanguage(string culture, string returnUrl)
         {
