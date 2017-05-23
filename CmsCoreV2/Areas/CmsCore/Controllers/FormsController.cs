43635cfee0ed7a3108c992281dd2c304c26f32fa
microsoftv3 <stronghuman0207@gmail.com>
2017-05-23 12:04:52 +0300

    FeedbackControllerAdded, FormFieldsControllerAdded, FormsControllerAdded, ViewsAdded, MetronicLayoutFixedForDatatable

---
+++ CmsCoreV2/Areas/CmsCore/Controllers/FormsController.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CmsCoreV2.Data;
+using CmsCoreV2.Models;
+
+namespace CmsCoreV2.Areas.CmsCore.Controllers
+{
+    [Area("CmsCore")]
+    public class FormsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FormsController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: CmsCore/Forms
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Forms.Include(f => f.Language);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: CmsCore/Forms/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var form = await _context.Forms
+                .Include(f => f.Language)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (form == null)
+            {
+                return NotFound();
+            }
+
+            return View(form);
+        }
+
+        // GET: CmsCore/Forms/Create
+        public IActionResult Create()
+        {
+            ViewData["LanguageId"] = new SelectList(_context.Languages, "Id", "Id");
+            return View();
+        }
+
+        // POST: CmsCore/Forms/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FormName,EmailTo,EmailBcc,EmailCc,Description,Template,ClosingDescription,GoogleAnalyticsCode,IsPublished,LanguageId,Id,CreateDate,CreatedBy,UpdateDate,UpdatedBy,AppTenantId")] Form form)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(form);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["LanguageId"] = new SelectList(_context.Languages, "Id", "Id", form.LanguageId);
+            return View(form);
+        }
+
+        // GET: CmsCore/Forms/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var form = await _context.Forms.SingleOrDefaultAsync(m => m.Id == id);
+            if (form == null)
+            {
+                return NotFound();
+            }
+            ViewData["LanguageId"] = new SelectList(_context.Languages, "Id", "Id", form.LanguageId);
+            return View(form);
+        }
+
+        // POST: CmsCore/Forms/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("FormName,EmailTo,EmailBcc,EmailCc,Description,Template,ClosingDescription,GoogleAnalyticsCode,IsPublished,LanguageId,Id,CreateDate,CreatedBy,UpdateDate,UpdatedBy,AppTenantId")] Form form)
+        {
+            if (id != form.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(form);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FormExists(form.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            ViewData["LanguageId"] = new SelectList(_context.Languages, "Id", "Id", form.LanguageId);
+            return View(form);
+        }
+
+        // GET: CmsCore/Forms/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var form = await _context.Forms
+                .Include(f => f.Language)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (form == null)
+            {
+                return NotFound();
+            }
+
+            return View(form);
+        }
+
+        // POST: CmsCore/Forms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var form = await _context.Forms.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Forms.Remove(form);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool FormExists(long id)
+        {
+            return _context.Forms.Any(e => e.Id == id);
+        }
+    }
+}
