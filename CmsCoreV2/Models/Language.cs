7e7916b2e65762789d624b829dc10899760b7c59
microsoftv3 <stronghuman0207@gmail.com>
2017-05-24 10:20:24 +0300

    LanguagesAndResourcesControllerAdded

--- CmsCoreV2/Models/Language.cs
+++ CmsCoreV2/Models/Language.cs
@@ -11,13 +11,19 @@ namespace CmsCoreV2.Models
         {
             IsActive = true;
             Pages = new HashSet<Page>();
-            
+            Posts = new HashSet<Post>();
+            PostCategories = new HashSet<PostCategory>();
+            Menus = new HashSet<Menu>();
+            Forms = new HashSet<Form>();
         }
         public string Name { get; set; }
         public string NativeName { get; set; }
         public string Culture { get; set; }
         public bool IsActive { get; set; }
         public virtual ICollection<Page> Pages { get; set; }
-
+        public virtual ICollection<Post> Posts { get; set; }
+        public virtual ICollection<PostCategory> PostCategories { get; set; }
+        public virtual ICollection<Menu> Menus { get; set; }
+        public virtual ICollection<Form> Forms { get; set; }
     }
 }
