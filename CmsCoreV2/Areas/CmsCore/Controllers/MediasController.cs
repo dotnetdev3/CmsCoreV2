4db227d327d6b108b518d6787ef378bea6e91754
microsoftv3 <stronghuman0207@gmail.com>
2017-05-26 12:44:57 +0300

    Media Popup İn Galertyitem

--- CmsCoreV2/Areas/CmsCore/Controllers/MediasController.cs
+++ CmsCoreV2/Areas/CmsCore/Controllers/MediasController.cs
@@ -53,10 +53,110 @@ namespace CmsCoreV2.Areas.CmsCore.Controllers
         }
 
         // GET: CmsCore/Medias/Create
-        public IActionResult Create()
+        public IActionResult Create(string element = "")
         {
+            if (HttpContext.Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                ViewBag.Element = element;
+                return View("ModalCreate");
+            }
             return View();
         }
+
+        public JsonResult ModalCreate(IFormFile uploadFile)
+        {
+            //IFormFileCollection uploadedFiles = Request.Form.Files;
+            //IFormFile uploadedFile = uploadedFiles[0];
+            IFormFile file = Request.Form.Files[0];
+            if (ModelState.IsValid)
+            {
+                if (uploadFile != null)
+                {
+                    Media media = new Media();
+                    media.FileName = uploadFile.FileName;
+                    media.Size = (uploadFile.Length / 1024);
+                    media.CreatedBy = User.Identity.Name ?? "username";
+                    media.CreateDate = DateTime.Now;
+                    media.UpdatedBy = User.Identity.Name ?? "username";
+                    media.UpdateDate = DateTime.Now;
+                    media.AppTenantId = tenant.AppTenantId;
+
+                    if (Path.GetExtension(uploadFile.FileName) == ".jpg" || Path.GetExtension(uploadFile.FileName) == ".jpeg" || Path.GetExtension(uploadFile.FileName) == ".png")
+                    {
+                        media.FileType = "Image";
+                    }
+                    else if (Path.GetExtension(uploadFile.FileName) == ".mp4" || Path.GetExtension(uploadFile.FileName) == ".gif")
+                    {
+                        media.FileType = "Video";
+                    }
+                    else
+                    {
+                        media.FileType = "Document";
+                    }
+
+                    if (Path.GetExtension(uploadFile.FileName) == ".doc"
+                    || Path.GetExtension(uploadFile.FileName) == ".pdf"
+                    || Path.GetExtension(uploadFile.FileName) == ".rtf"
+                    || Path.GetExtension(uploadFile.FileName) == ".docx"
+                    || Path.GetExtension(uploadFile.FileName) == ".jpg"
+                    || Path.GetExtension(uploadFile.FileName) == ".gif"
+                    || Path.GetExtension(uploadFile.FileName) == ".png"
+                    || Path.GetExtension(uploadFile.FileName) == ".mp4"
+                    || Path.GetExtension(uploadFile.FileName) == ".mp4"
+                     )
+                    {
+                        string category = DateTime.Now.Month + "-" + DateTime.Now.Year;
+                        string FilePath = UploadPath + category + "\\";
+                        string dosyaismi = Path.GetFileName(uploadFile.FileName);
+                        var yuklemeYeri = Path.Combine(FilePath, dosyaismi);
+                        media.FileUrl = "uploads/" + category + "/";
+                        try
+                        {
+                            if (!Directory.Exists(FilePath))
+                            {
+                                Directory.CreateDirectory(FilePath);//Eðer klasör yoksa oluþtur    
+                            }
+                            using (var stream = new FileStream(yuklemeYeri, FileMode.Create))
+                            {
+                                uploadFile.CopyToAsync(stream);
+                            }
+
+
+                            _context.Add(media);
+                            _context.SaveChangesAsync();
+                            return Json(new { result = AssetsUrl + media.FileUrl + media.FileName });
+                        }
+                        catch (Exception exc) { ModelState.AddModelError("FileName", "Hata: " + exc.Message); }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("FileName", "Dosya uzantýsý izin verilen uzantýlardan olmalýdýr.");
+                    }
+                }
+                else { ModelState.AddModelError("FileExist", "Lütfen bir dosya seçiniz!"); }
+            }
+            return Json(new { result = "false" });
+        }
+        
+        public IEnumerable<Media> MediaGallery(string word, int? year, int? month, string category)
+        {
+            var mediagallery = _context.Medias.Where(w => w.CreateDate.Year == year && w.CreateDate.Month == month && w.FileType == category).ToList();
+
+            if (!string.IsNullOrEmpty(word))
+            {
+                mediagallery = mediagallery.Where(w => w.Title.Contains(word) || w.Description.Contains(word) || w.FileName.Contains(word)).ToList();
+            }
+            return mediagallery;
+        }
+    
+
+    public JsonResult ModalGallery(string word, int year, int month, string category)
+        {
+            var mediagallery= MediaGallery(word, year, month, category);
+            return Json(new { result = mediagallery });
+        }
+
+
 
         // POST: CmsCore/Medias/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
