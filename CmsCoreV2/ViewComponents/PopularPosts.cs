a7ec0ff7043f56154c7465fdc42ae1cf09f638a2
microsoftv3 <stronghuman0207@gmail.com>
2017-05-29 12:46:49 +0300

    popularPosts edited

---
+++ CmsCoreV2/ViewComponents/PopularPosts.cs
@@ -0,0 +1,47 @@
+﻿using CmsCoreV2.Data;
+using CmsCoreV2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsCoreV2.ViewComponents
+{
+    public class PopularPosts : ViewComponent
+
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<PopularPosts> dbSet;
+
+
+        public PopularPosts(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int total, long id)
+        {
+            var items = await Popular(total, id);
+            return View(items);
+        }
+        public Task<List<Post>> Popular(int total, long id)
+        {
+            return Task.FromResult(PopulerPost(total, id).Where(w => w.IsPublished == true).ToList());
+        }
+        public IEnumerable<Post> PopulerPost(int total, long id)
+        {
+            var post = GetAll().Where(u => u.Id != id).OrderByDescending(m => m.ViewCount).Take(total).ToList();
+            return post;
+        }
+        public virtual IEnumerable<Models.Post> GetAll(params string[] navigations)
+        {
+            var set = _context.Posts.AsQueryable();
+            foreach (string nav in navigations)
+                set = set.Include(nav);
+            return set.AsEnumerable();
+        }
+
+
+    }
+}
